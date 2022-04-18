5478c2bb3eeacc18b873695f373ceb059f98221b
Shinonome <cuulbulu@gmail.com>
2022-04-18 10:50:15 +0900

    Update: userの名前にバリデーションかけた

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { User.new(name: 'Example User', email: 'user@example.com') }
+
+  it 'userが有効であること' do
+    expect(user).to be_valid
+  end
+
+  it 'nameが必須であること' do
+    user.name =''
+    expect(user).to_not be_valid 
+  end
+end
