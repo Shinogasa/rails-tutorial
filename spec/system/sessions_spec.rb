eb7864bb37accdee929025f2f079693f795d1487
Shinonome <cuulbulu@gmail.com>
2022-04-21 13:25:41 +0900

    Update: レイアウト変更のテスト追加

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -5,6 +5,22 @@ RSpec.describe "Sessions", type: :system do
     driven_by(:rack_test)
   end
  
+  context '有効な値の場合' do
+    let(:user) { FactoryBot.create(:user) }
+   
+    it 'ログインユーザ用のページが表示されること' do
+      visit login_path
+   
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+   
+      expect(page).to_not have_selector "a[href=\"#{login_path}\"]"
+      expect(page).to have_selector "a[href=\"#{logout_path}\"]"
+      expect(page).to have_selector "a[href=\"#{user_path(user)}\"]"
+    end
+  end
+  
   describe '#new' do
     context '無効な値の場合' do
       it 'flashメッセージが表示される' do
