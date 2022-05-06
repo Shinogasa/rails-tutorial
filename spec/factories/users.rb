00c9885f29bdfe57dc329badf49f6be8cd69f629
Shinonome <cuulbulu@gmail.com>
2022-05-06 16:02:11 +0900

    Update: モデルに有効化の状態を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,8 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
     admin { true }
+    activated { true }
+    activated_at { Time.now }
   end
 
   factory :archer, class: User do
@@ -12,6 +14,8 @@ FactoryBot.define do
     email { 'duchess@example.gov' }
     password { 'password' }
     password_confirmation { 'password' }
+    activated { true }
+    activated_at { Time.now }
   end
 
   factory :continuous_users, class: User do
@@ -19,5 +23,7 @@ FactoryBot.define do
     sequence(:email) { |n| "user-#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    activated { true }
+    activated_at { Time.now }
   end
 end
