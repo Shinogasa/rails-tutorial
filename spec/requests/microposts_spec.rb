24ab2ea7a5ef8cc342c1d0cc49add01950dea1eb
Shinonome <cuulbulu@gmail.com>
2022-05-17 15:41:37 +0900

    Update: マイクロポスト投稿フォーム周り

---
+++ spec/requests/microposts_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+ 
+RSpec.describe "Microposts", type: :request do
+  describe '#create' do
+    it '無効な値だと登録されないこと' do
+      expect {
+        post microposts_path, params: { micropost: { content: 'Lorem ipsum' } }
+      }.to_not change(Micropost, :count)
+    end
+
+    it 'ログインページにリダイレクトされること' do
+      post microposts_path, params: { micropost: { content: 'Lorem ipsum' } }
+      expect(response).to redirect_to login_path
+    end
+  end
+  
+  describe '#destroy' do
+    let(:user) { FactoryBot.create(:archer) }
+ 
+    before do
+      @post = FactoryBot.create(:most_recent)
+    end
+ 
+    context '他のユーザの投稿を削除した場合' do
+      before do
+        log_in user
+      end
+ 
+      it '削除されないこと' do
+        expect {
+          delete micropost_path(@post)
+        }.to_not change(Micropost, :count)
+      end
+ 
+      it 'ログインページにリダイレクトされること' do
+        delete micropost_path(@post)
+        expect(response).to redirect_to login_path
+      end
+    end
+ 
+    context '未ログインの場合' do
+      it '削除されないこと' do
+        expect {
+          delete micropost_path(@post)
+        }.to_not change(Micropost, :count)
+      end
+ 
+      it 'ログインページにリダイレクトされること' do
+        delete micropost_path(@post)
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+end
