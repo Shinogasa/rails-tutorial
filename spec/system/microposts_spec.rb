7636f9f9b56a8fff12e2674808400e6b595d2bb6
Shinonome <cuulbulu@gmail.com>
2022-05-20 15:20:51 +0900

    Update: 画像リサイズまで

--- spec/system/microposts_spec.rb
+++ spec/system/microposts_spec.rb
@@ -4,6 +4,73 @@ RSpec.describe "MicroPosts", type: :system do
   before do
     driven_by(:rack_test)
   end
+
+  describe 'home' do
+    before do
+      FactoryBot.send(:user_with_posts, posts_count: 35)
+      @user = Micropost.first.user
+      @user.password = 'password'
+      log_in @user
+      visit root_path
+    end
+   
+    it 'ページネーションのラッパータグがあること' do
+      expect(page).to have_selector 'div.pagination'
+    end
+   
+    # context '有効な送信の場合' do
+    #   it '投稿されること' do
+    #     content = 'This micropost really ties the room toghether'
+    #     fill_in 'micropost_content', with: content
+    #     expect{click_on "Post"}.to change ( Micropost, :count ).from(35).to(36)
+   
+    #     expect(page).to have_content 'This micropost really ties the room together'
+    #   end
+    # end
+
+    # it '画像添付ができること' do
+    #   expect {
+    #     fill_in 'micropost_content', with: 'This micropost really ties the room together'
+    #     attach_file 'micropost[picture]', "#{Rails.root}/spec/factories/kitten.jpg"
+    #     click_button 'Post'
+    #   }.to change(Micropost, :count).by 1
+     
+    #   attached_post = Micropost.first
+    #   expect(attached_post.picture).to be_attached
+    # end
+   
+    # context '無効な送信の場合' do
+    #   it 'contentが空なら投稿されないこと' do
+    #     fill_in 'micropost_content', with: ''
+    #     click_on 'Post'
+  
+    #     expect(page).to have_selector 'div#error_explanation'
+    #     expect(page).to have_link '2', href: '/?page=2'
+    #   end
+    # end
+   
+    describe '削除機能' do
+      # it 'deleteボタンが表示されていること' do
+      #   expect(page).to have_link 'delete'
+   
+      #   fill_in 'micropost_content', with: 'This micropost really ties the room together'
+      #   click_button 'Post'
+   
+      #   post = Micropost.first
+   
+      #   expect {
+      #     click_link 'delete', href: micropost_path(post)
+      #   }.to change(Micropost, :count).by -1
+      #   expect(page).to_not have_content 'This micropost really ties the room together'
+      # end
+   
+      it '他のユーザのプロフィールではdeleteボタンが表示されないこと' do
+        @other_user = FactoryBot.create(:archer)
+        visit user_path(@other_user)
+        expect(page).to_not have_link 'delete'
+      end
+    end
+  end
  
   describe 'Users#show' do
     before do
